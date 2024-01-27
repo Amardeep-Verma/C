4a18ff83a1eda5058e58ced38fc9b44fcaf64f10
Amardeep <vermaamardeep86@gmail.com>
2024-01-27 15:04:41 +0530

    Day 2 C

--- condiotional_statements.c
+++ condiotional_statements.c
@@ -40,7 +40,7 @@ int main() {
     return 0;
 }
 
-*/
+
 
 
 #include <stdio.h>
@@ -90,6 +90,124 @@ int main() {
         printf("Maximum: %d\n", num2);
     } else {
         printf("Maximum: %d\n", num3);
+    }
+
+    return 0;
+}
+
+
+
+
+// TERNARY OPERATOR 
+
+// SYNTAX - Condition ? doSomething if TRUE : doSomething if FALSE;
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("Enter your age :");
+    scanf("%d",&age);
+    age >=18 ? printf("Adult \n") : printf("not adult \n");
+    return 0;
+
+}
+
+ 
+
+// SWITCH CASE
+
+#include<stdio.h>
+int main()
+{
+     int day;
+     printf("Enter day : ");
+     scanf("%d",&day);
+     switch(day){
+         case 1 : printf("Monday");
+         break;
+         case 2 : printf("Tuesday");
+         break;
+         case 3 : printf("Wednesday ");
+         break;
+         case 4 : printf("Thrusday");
+         break;
+         default : printf("Enter a valid day ");
+         break;
+     }
+}
+
+
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+    if(num >= 0){
+        printf("Positive\n");
+        if (num % 2 == 0){
+            printf("Even");
+        }else{
+            printf("Negative");
+        }
+    }
+}
+
+
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("Enter Marks : ");
+    scanf("%d",&marks);
+    if (marks <= 30){
+        printf("Fail");
+    }else{
+      printf("Pass");
+    }
+}
+
+*/
+
+// #include<stdio.h>
+// int main()
+// {
+//     int marks;
+//     printf("Enter marks(0-100)");
+//     scanf("%d",&marks);
+
+//     if(marks >=0 && marks <=30){
+//         printf("FAIL\n");
+//     }else if(marks >30 && marks <=100){
+//         printf("PASS\n");
+//     }else{
+//         printf("Wrong marks ");
+
+ 
+//     }
+//     return 0;
+// }
+
+#include<stdio.h>
+
+int main() {
+    int marks;
+
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks < 30) {
+        printf("GRADE C\n");
+    } else if (marks >= 30 && marks < 70) {
+        printf("GRADE B\n");
+    } else if (marks >= 70 && marks < 90) {
+        printf("GRADE A\n");
+    } else if (marks >= 90 && marks <= 100) {
+        printf("GRADE A+\n");
+    } else {
+        printf("Wrong marks entered\n");
     }
 
     return 0;
