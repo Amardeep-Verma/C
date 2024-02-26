be32ca9e11edc9912e50d9cbbef0e5d8012a0a6c
Amardeep <vermaamardeep86@gmail.com>
2024-02-26 20:09:18 +0530

    New programs

--- condiotional_statements.c
+++ condiotional_statements.c
@@ -1565,3 +1565,22 @@ n=n/10;
 }    
 return 0;  
 }
+
+#include <stdio.h>
+int main() {
+  long number, temp;
+  int count = 0;
+  printf("Enter an integer: ");
+  scanf("%ld", &number);
+
+
+  temp = number;
+
+
+  do {
+    temp /= 10;
+    ++count;
+  } while (temp != 0);
+
+  printf("Number of digits in the number %ld is: %d", number,count);
+}
