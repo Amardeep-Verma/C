9006a4d69da9b0aabd5d7d0dabbe4e08c11481b9
Amardeep <vermaamardeep86@gmail.com>
2024-03-16 23:10:31 +0530

    Practices

--- new.c
+++ new.c
@@ -61,3 +61,37 @@ int main()
     }
     greeting();
 }
+
+// program to find roots of quadratic equation
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a, b, c, d;
+    float x, y;
+    printf("Enter the value of a,b & c :");
+    scanf("%d%d%d", &a, &b, &c);
+    d = b * b - 4 * a * c; // discriminant formula
+
+    if (d == 0)
+    {
+        printf("Both roots are equal.\n");
+        x = -b / (2.0 * a);
+        y = x;
+    }
+    else if (d > 0)
+    {
+        printf("Roots are unequal.\n");
+        x = (-b + sqrt(d)) / (2 * a);
+        y = (-b - sqrt(d)) / (2 * a);
+        printf("First root = %f\n", x);
+        printf("Second root = %f\n", y);
+    }
+    else
+    {
+        printf("Roots are imaginary");
+    }
+    return 0;
+}
