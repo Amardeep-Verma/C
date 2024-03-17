96defcc8f21ab6427de094ca5b559ece9f133910
Amardeep <vermaamardeep86@gmail.com>
2024-03-17 16:51:42 +0530

    Practices

---
+++ Function.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+// int areaOfRect(int length, int breadth)
+// {
+//     int area;
+//     area = length * breadth;
+//     return area;
+// }
+// int main()
+// {
+//     int l = 10, b = 5;
+//     int area = areaOfRect(l, b);
+//     printf("%d", area);
+// }
+
+// #include <stdio.h>
+// char fun();
+
+// int main()
+// {
+//     char c = fun();
+//     printf("Character is %c", c);
+// }
+// char fun()
+// {
+//     return 'a';
+// }
